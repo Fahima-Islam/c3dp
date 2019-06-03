f7f06a56cbe8df9230143947756ee786384d0266
Fahima-Islam <31892119+Fahima-Islam@users.noreply.github.com>
2019-06-03 10:38:02 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [  'future', 'cython', 'numpy', 'scipy', 'pandas', 'h5py', 'matplotlib',
-    'lmfit', 'mdtraj', 'MDAnalysis', 'tqdm' ]
+requirements = [ 'numpy', 'scipy',  'matplotlib','lmfit' ]
 
 setup_requirements = ['pytest-runner', ]
 
