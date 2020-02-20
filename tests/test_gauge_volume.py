9e7e8187fbe1851d9d508b5bde18b07db8f41683
Islam, Fahima F <islamff@ornl.gov>
2020-02-20 18:43:56 -0500

    correcting the import

--- tests/test_gauge_volume.py
+++ tests/test_gauge_volume.py
@@ -2,7 +2,7 @@ import numpy as np
 
 import unittest
 from numpy.testing import assert_allclose
-from gaugevol import gauge_volume_allchannels as gv
+from c3dp.gaugevol import gauge_volume_allchannels as gv
 
 
 class TestGaugeVolume(object):
