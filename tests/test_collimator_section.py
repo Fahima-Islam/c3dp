cd0c5c1dd9aae9d57010b4ec19a75f9fd3e2760a
Islam, Fahima F <islamff@ornl.gov>
2020-03-04 22:30:59 -0500

    add init function

--- tests/test_collimator_section.py
+++ tests/test_collimator_section.py
@@ -5,21 +5,20 @@ import pytest
 
 
 class TestCollimatorSection(object):
+    def __init__(self):
+        self.small_section = CollimatorSection(12, 50, 10)
+
     def test_n_blades_from_thickness(self):
-        small_section = CollimatorSection(12, 50, 10)
-        np.testing.assert_allclose(small_section.n_blades_from_thickness(3), 1, atol=0.01)
+        np.testing.assert_allclose(self.small_section.n_blades_from_thickness(3), 1, atol=0.01)
 
     def test_channel_thickness(self):
-        small_section = CollimatorSection(12, 50, 10)
-        np.testing.assert_allclose(small_section.chanel_thickness(10), 0.0, atol=0.01)
+        np.testing.assert_allclose(self.small_section.chanel_thickness(10), 0.0, atol=0.01)
 
     def test_set_blade_angles(self):
-        small_section = CollimatorSection(12, 50, 10)
-        np.testing.assert_allclose(max(small_section.set_blade_angles(10)), 0.0, atol=0.01)
+        np.testing.assert_allclose(max(self.small_section.set_blade_angles(10)), 0.0, atol=0.01)
 
     def test_blade_blade_angle_distances(self):
-        small_section = CollimatorSection(12, 50, 10)
-        np.testing.assert_allclose(small_section.blade_blade_angle_distances(np.array([[4, 3], [4, 3]])),
+        np.testing.assert_allclose(self.small_section.blade_blade_angle_distances(np.array([[4, 3], [4, 3]])),
                                    0.0, atol=0.01)
 
     Triad = namedtuple('Triad', 'blades d widths')
