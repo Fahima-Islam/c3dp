c53a8d9fc8d8d9f6ad2eb3ffffd0110ab1da02b9
Islam, Fahima F <islamff@ornl.gov>
2020-03-04 20:06:24 -0500

    added new test

--- tests/test_collimator_section.py
+++ tests/test_collimator_section.py
@@ -1,5 +1,7 @@
-from c3dp.instrument.collimator.section import CollimatorSection
+from c3dp.instrument.collimator.section import CollimatorSection, blade_configurations
 import numpy as np
+from collections import namedtuple
+import pytest
 
 
 class TestCollimatorSection(object):
@@ -19,3 +21,9 @@ class TestCollimatorSection(object):
         small_section = CollimatorSection(12, 50, 10)
         np.testing.assert_allclose(small_section.blade_blade_angle_distances(np.array([[4, 3], [4, 3]])),
                                    0.0, atol=0.01)
+
+    Triad = namedtuple('Triad', 'blades d widths')
+
+    @pytest.mark.parametrize('expected', [Triad([1, 2, 1], 10.47, [6.80, 19.88, 53.62])])
+    def test_blade_configurations(self, expected):
+        np.testing.assert_allclose(blade_configurations(20, 60, 45)[0][0], expected[0], atol=0.01)
