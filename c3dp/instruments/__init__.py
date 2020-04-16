98ef0a3a4c7da595afd9fe3d132b89303cd9dfec
Islam, Fahima F <islamff@ornl.gov>
2020-04-15 22:28:45 -0400

    add the instrument init

--- c3dp/instruments/__init__.py
+++ c3dp/instruments/__init__.py
@@ -1,3 +1,5 @@
 import pkgutil
-
-DEFAULT_INSTRUMENT_CONFIGURATION  = pkgutil.get_loader("c3dp/instruments/myinstrument_multipleDetectors").get_filename()
+import os
+this_dir = os.path.dirname(__file__)
+myistrument_dir = os.path.join (this_dir,'myinstrument_multipleDetectors' )
+DEFAULT_INSTRUMENT_CONFIGURATION  = pkgutil.get_loader(myistrument_dir).get_filename()
