f8077a4b58a90a8030b02b5e4a3759da2cc8fe9a
Islam, Fahima F <islamff@ornl.gov>
2020-04-16 10:01:41 -0400

    add sample peaks

---
+++ sample/Al_peaks.py
@@ -0,0 +1,13 @@
+from mccomponents.sample.diffraction.SimplePowderDiffractionKernel import Peak
+peaks = [
+    Peak(q=2.6872760504735616, F_squared=1.779379382704264, multiplicity=8, intrinsic_line_width=0, DebyeWaller_factor=0),
+    ]
+
+# unit: \AA
+unitcell_volume = 66.4178238844
+
+# unit: barns
+class cross_sections:
+    coh = 0
+    inc = 0
+    abs = 0.924
---
+++ sample/Si_peaks.py
@@ -0,0 +1,13 @@
+from mccomponents.sample.diffraction.SimplePowderDiffractionKernel import Peak
+peaks = [
+    Peak(q=2.0040025900612473, F_squared=5.420969307916806, multiplicity=8, intrinsic_line_width=0, DebyeWaller_factor=0),
+    ]
+
+# unit: \AA
+unitcell_volume = 160.149892567
+
+# unit: barns
+class cross_sections:
+    coh = 0
+    inc = 0
+    abs = 1.368
