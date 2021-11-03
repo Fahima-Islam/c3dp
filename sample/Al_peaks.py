76403aaa4affcab4450f0dbbf463eea5e5b436c2
Fahima Islam <31892119+Fahima-Islam@users.noreply.github.com>
2021-11-03 11:49:33 -0400

    Al_peaks

---
+++ sample/Al_peaks.py
@@ -0,0 +1,33 @@
+from mccomponents.sample.diffraction.SimplePowderDiffractionKernel import Peak
+    
+peaks = [
+    Peak(q=2.6872760504735616, F_squared=1.779379382704264, multiplicity=8, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=3.1029991022554904, F_squared=1.7398932975288408, multiplicity=6, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=4.388303414441253, F_squared=1.5905189903739325, multiplicity=12, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=5.145741873495554, F_squared=1.4869660112543794, multiplicity=24, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=5.374552100947123, F_squared=1.453968851039947, multiplicity=8, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=6.205998204510981, F_squared=1.3291418917905624, multiplicity=6, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=6.7628297543145575, F_squared=1.2426062368247879, multiplicity=24, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=6.938516926764098, F_squared=1.2150316475136496, multiplicity=24, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=7.6007644728402335, F_squared=1.110718060711279, multiplicity=24, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=8.061828151420684, F_squared=1.0384031969186098, multiplicity=24, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=8.061828151420684, F_squared=1.0384031969186098, multiplicity=8, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=8.776606828882507, F_squared=0.9281887945975766, multiplicity=12, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=9.17879512791498, F_squared=0.8677577557683145, multiplicity=48, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=9.308997306766472, F_squared=0.8485014032507151, multiplicity=24, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=10.173862927002336, F_squared=0.7251552430987769, multiplicity=24, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=10.749104201894246, F_squared=0.6481884618472749, multiplicity=8, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=11.07992300129515, F_squared=0.6059872390631159, multiplicity=24, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=11.917294179964934, F_squared=0.5064026460570142, multiplicity=24, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=13.436380252367806, F_squared=0.3536396411307667, multiplicity=8, intrinsic_line_width=0, DebyeWaller_factor=0),
+    ]
+    
+    # unit: \AA
+unitcell_volume = 66.4178238844
+    
+    # unit: barns
+class cross_sections:
+    coh = 0
+    inc = 0
+    abs = 0.924
+    
