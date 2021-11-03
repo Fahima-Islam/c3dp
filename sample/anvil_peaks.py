b607c168151357bba4a3532b376692f55e5b0bcc
Fahima Islam <31892119+Fahima-Islam@users.noreply.github.com>
2021-11-03 11:29:21 -0400

    anvil peaks

---
+++ sample/anvil_peaks.py
@@ -0,0 +1,29 @@
+from mccomponents.sample.diffraction.SimplePowderDiffractionKernel import Peak
+    
+peaks = [
+        Peak(q=3.0508634936322037, F_squared=13.945739875438191, multiplicity=8, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=4.982039222855823, F_squared=27.241669703961758, multiplicity=12, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=5.841958821735356, F_squared=13.42953661195265, multiplicity=24, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=7.0456674372374195, F_squared=26.23331597518273, multiplicity=6, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=7.677838087178278, F_squared=12.932440675264601, multiplicity=24, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=8.62914505928725, F_squared=25.262286582738167, multiplicity=24, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=9.15259048089661, F_squared=12.453744805340744, multiplicity=24, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=9.152590480896611, F_squared=12.453744805340754, multiplicity=8, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=9.964078445711646, F_squared=24.327199961763398, multiplicity=12, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=10.420682670970894, F_squared=11.99276792146416, multiplicity=48, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=11.5503827705902, F_squared=11.548854153203786, multiplicity=24, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=12.203453974528815, F_squared=22.559582575816503, multiplicity=8, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=12.579032433586487, F_squared=11.121371907252664, multiplicity=24, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=13.52969961910813, F_squared=10.709712968807162, multiplicity=24, intrinsic_line_width=0, DebyeWaller_factor=0),
+    Peak(q=15.254317468161018, F_squared=9.931543887617401, multiplicity=8, intrinsic_line_width=0, DebyeWaller_factor=0),
+        ]
+    
+    # unit: \AA
+unitcell_volume = 45.3892658731
+    
+    # unit: barns
+class cross_sections:
+	coh = 0
+	inc = 0
+	abs = 0.028
+    
